0d7742bc48b76bab5c7d23909e8c2ca8f39f2ab1
Adrian Stein <adrian.stein@tum.de>
2023-12-15 14:30:11 +0100

    added bash config

---
+++ bash/link.jl
@@ -0,0 +1,21 @@
+link_target_path = "$(@__DIR__)/.bashrc"
+
+if Sys.islinux()
+    config_path = "$(ENV["HOME"])/.bashrc"
+else
+    println("OS not yet supported")
+    exit()
+end
+
+if ispath(config_path)
+    print("Config already exists, overwrite? [y/N]: ")
+    answer = readline()
+    if answer == "y" || answer == "Y"
+        rm(config_path)
+    else
+        exit()
+    end
+end
+
+symlink(link_target_path, config_path)
+println("Symlink created")
