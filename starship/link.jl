ad44906b2b869baafa85c4fe09e46c5fd8104a5b
Adrian Stein <adrian.stein@tum.de>
2024-03-31 20:20:25 +0200

    added preliminary starship config

---
+++ starship/link.jl
@@ -0,0 +1,21 @@
+link_target_path = "$(@__DIR__)/starship.toml"
+
+if Sys.islinux()
+    config_path = "$(ENV["HOME"])/.config/starship.toml"
+else
+    println("OS not yet supported")
+    exit()
+end
+
+if ispath(config_path)
+    print("Config already exists, overwrite? [y/N]: ")
+    answer = readline()
+    if answer == "y" || answer == "Y"
+        rm(config_path)
+    else
+        exit()
+    end
+end
+
+symlink(link_target_path, config_path)
+println("Symlink created")
