b3ba0675fdcb74cc500a7c4dc8af5c6b44d8ba3b
Adrian Stein <adrian.stein@tum.de>
2024-04-09 21:21:41 +0200

    added sublime conf

---
+++ sublime/README.md
@@ -0,0 +1,5 @@
+# Sublime Install Instructions
+
+- **Linux**: [Official Instructions](https://www.sublimetext.com/docs/linux_repositories.html)
+- **Windows**: `scoop install sublime-text`
+
---
+++ sublime/link.jl
@@ -0,0 +1,23 @@
+link_target_path = "$(@__DIR__)/User"
+
+if Sys.iswindows()
+    config_path = "$(ENV["USERPROFILE"])/scoop/persist/sublime-text/Data/Packages/User"
+elseif Sys.islinux()
+    config_path = "$(ENV["HOME"])/.config/sublime-text/Packages/User"
+else
+    println("OS not yet supported")
+    exit()
+end
+
+if ispath(config_path)
+    print("Config already exists, overwrite? [y/N]: ")
+    answer = readline()
+    if answer == "y" || answer == "Y"
+        rm(config_path, recursive=true)
+    else
+        exit()
+    end
+end
+
+symlink(link_target_path, config_path, dir_target=true)
+println("Symlink created")
