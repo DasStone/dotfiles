473b062fdcef19008b22cfefa3dce5c0e330e16f
Adrian Stein <adrian.stein@tum.de>
2024-04-20 10:41:03 +0200

    added fish config

---
+++ fish/config.fish
@@ -0,0 +1,34 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    
+    # remove greeting
+    set -g fish_greeting
+
+    function fish_user_key_bindings
+        fish_default_key_bindings -M insert
+        fish_vi_key_bindings --no-erase insert
+    end
+
+    # Powerline
+    starship init fish | source
+
+    # Path
+    fish_add_path -g "$HOME/.local/bin/"
+    fish_add_path -g "$HOME/Documents/isabelle/isabelle-emacs/bin/"
+    #fish_add_path -g "$HOME/.cargo/bin/"
+
+
+    # alias
+    alias ls "ls --color=auto"
+    alias grep "grep --color=auto"
+
+    alias hacked "cmatrix -s -M 'You\'ve been hacked ¯\\_(ツ)_/¯'"
+    alias wisdom "fortune | cowsay"
+    alias present "impressive -q -t None"
+    alias clock "tty-clock -c -C 3"
+
+    alias deb "sudo docker run -ti --rm debian:latest bash"
+
+    # Env
+    set -gx EDITOR nvim
+end
