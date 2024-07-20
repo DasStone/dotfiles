b3accb91a299b99d33c7a19d9b86a4a3bc3f4f6b
Adrian Stein <adrian.stein@tum.de>
2024-07-20 12:55:15 +0200

    added preliminary kitty config

---
+++ kitty/link.jl
@@ -0,0 +1,21 @@
+link_target_path = "$(@__DIR__)/kitty"
+
+if Sys.islinux()
+    config_path = "$(ENV["HOME"])/.config/kitty"
+else
+    println("OS not yet supported")
+    exit()
+end
+
+if ispath(config_path)
+    print("Config already exists, overwrite? [y/N]: ")
+    answer = readline()
+    if answer == "y" || answer == "Y"
+        rm(config_path, recursive=true)
+    else
+        exit()
+    end
+end
+
+symlink(link_target_path, config_path, dir_target=true)
+println("Symlink created")
