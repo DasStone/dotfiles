1a35a070ef119ca5cc35a2533d983ab58d73d207
Adrian Stein <adrian.stein@tum.de>
2024-10-26 10:30:38 +0200

    updated hyprland

--- hyprland/hypr/scripts/greetings.py
+++ hyprland/hypr/scripts/greetings.py
@@ -2,17 +2,17 @@
 
 import random
 
+def construct_train():
+    num_wagons = random.randint(0, 10)
+    return "🚂" + "🚃" * num_wagons
+
 greetings = [
     "Hey there, Adrian. The journey continues!",
     "Here again Adrian? Ready for the next stop?",
     "Hey there, Adrian. Ready for departue?",
     "Full steam ahead, Adrian!",
     "Welcome aboard, Adrian!",
+    construct_train()
 ]
 
-def construct_train():
-    num_wagons = random.randint(0, 10)
-    return "🚂" + "🚃" * num_wagons
-
-
-print(random.choice([random.choice(greetings), construct_train()]))
+print(random.choice(greetings))
