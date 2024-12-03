444148883a67c8f6a6a24a13bbbd0e75453989e0
Adrian Stein <adrian.stein@tum.de>
2024-12-03 19:01:17 +0100

    fixed env for devices; merged monitor and keybind settings

--- hyprland/link.py
+++ hyprland/link.py
@@ -15,11 +15,8 @@ make_symlink(target, link)
 # link specific device config
 base_path = join(target_dir(), "hypr")
 
-# monitors
-link_config_subfolder(base_path, "monitors", "Found monitor configurations:", "monitors.conf")
-
-# keybinds
-link_config_subfolder(base_path, "keybinds", "Found keybind configurations:", "keybinds.conf")
+# device
+link_config_subfolder(base_path, "devices", "Found device configurations:", "device.conf")
 
 # hyprpaper
 link_config_subfolder(base_path, "wallpapers", "Found wallpaper configurations:", "hyprpaper.conf")
